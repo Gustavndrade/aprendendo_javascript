232f2955e4d264579f06b17c5c5a9818e6086861
Gustavo Andrade <abguto.06@gmail.com>
2025-07-03 15:29:40 -0300

    estudando

--- interatividade/contador/script.js
+++ interatividade/contador/script.js
@@ -7,14 +7,22 @@ function Contador(){
      
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert("Valores invalidos, tente novamente")
-    }
+    } else{
     
-    var i = Number(inicio.value)
-    var f = Number(fim.value)
-    var p = Number(passo.value)
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
 
-    for(var c = i; c<= f ; c += p){
-        resultado.innerHTML += `${c}`;
-
+        if (i < f) {
+            for(var c = i; c<= f ; c += p){
+            resultado.innerHTML += `${c} \u{1F449}`;
+            }
+        }
+            else{
+                for(var c = i; c >= f ; c -= p){
+                resultado.innerHTML += `${c} \u{1F449}`;
+                }
+            }
+        resultado.innerHTML += `\u{1F3C1}`
     }
 }
